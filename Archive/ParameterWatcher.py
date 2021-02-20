50ef84f93ed99d280f76ab863f8f7da475840570
liumengyang <SENSETIME\liumengyang@cn0614006575l.domain.sensetime.com>
2021-02-20 16:07:45 +0800

    update

--- Archive/ParameterWatcher.py
+++ Archive/ParameterWatcher.py
@@ -1,5 +1,8 @@
 import json
+import os
+import time
 
+from multiprocessing import Process, Queue
 from hashlib import md5
 from functools import singledispatch
 from datetime import datetime
@@ -40,9 +43,63 @@ def handle_args_parser_params(args):
         arg_parse_dict[kwarg[0]] = arg_parse_dict[kwarg[1]]
         arg_parse_dict['insert_keys'].append(kwarg[0])
     return arg_parse_dict
+
 class ParameterWatcher(object):
 
-    def __init__(self):
+    @classmethod
+    def save_to_file(cls, save_path='./params'):
+        while True:
+            if cls.WATCHER_QUEUE.empty():
+                Logger.get_logger().info("Empty queue, skipping this round...")
+                time.sleep(2)
+                continue
+            watcher = cls.WATCHER_QUEUE.get()
+            whole_data = dict()
+            whole_data['name'] = watcher.name
+            whole_data['description'] = watcher.description
+            whole_data['model_parameters'] = watcher.model_parameters
+            whole_data['training_parameters'] = watcher.training_parameters
+            whole_data['data_parameters'] = watcher.data_parameters
+            whole_data['miscellaneous_parameters'] = watcher.data_parameters
+            whole_data['models'] = watcher.models
+            whole_data['results'] = watcher.results
+            hash_code = get_md5_hash(whole_data.__str__())
+            whole_data['time'] = watcher.time
+            whole_data['id'] = watcher.id
+            json_str = json.dumps(whole_data)
+            name = watcher.name + '_' + hash_code + '_' + watcher.time
+            os.makedirs(save_path, exist_ok=True)
+            Logger.get_logger().info("Saving json str {}".format(json_str))
+            with open(save_path + '/{}.json'.format(name), 'w') as f:
+                f.write(json_str)
+            
+    @classmethod
+    def run_save(cls):
+        if hasattr(cls, 'save_proc'):
+            Logger.get_logger().warning("Already have one saving process...")
+            return False
+        cls.save_proc = Process(target=cls.save_to_file)
+        # save_proc.daemon = True 
+        # Cannot set save process to daemon, otherwise save process will interupt once main thread exits.
+        cls.save_proc.start()
+        return True
+    
+    @classmethod
+    def terminate_save_proc(cls):
+        if cls.save_proc:
+            cls.save_proc.terminate()
+            return True
+        else:
+            Logger.get_logger().warning("No saving process can be terminated...")
+            return False
+    
+    def __new__(cls,*args,**kwargs):
+        if not hasattr(cls, 'WATCHER_QUEUE'):
+            cls.WATCHER_QUEUE = Queue()
+            cls.run_save()
+        return super().__new__(cls)
+
+    def __init__(self, name):
         self.model_parameters = dict()
         self.training_parameters = dict()
         self.miscellaneous_parameters = dict()
@@ -51,6 +108,8 @@ class ParameterWatcher(object):
         self.results = dict()
         self.id = get_md5_hash(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         self.time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.name = name
+        self.description = name
 
     def insert_parameter(self, dic, pkg):
         Logger.get_logger().info("Inserting parameter package.")
@@ -124,17 +183,16 @@ class ParameterWatcher(object):
             if DEV_MODE == DevelopMode.DEBUG:
                 raise Exception("As you are using DEBUG mode, this exception occured, please check context and fix it, or you can mute it by switching to RELEASE mode in CommonDefine.py")
         PARAMETER_OPERATION_DISPATCHER_THEME[pkg['parameter_type']](pkg)
-    
-
-    
+        ParameterWatcher.WATCHER_QUEUE.put(self, block=False)
 
 
 if __name__ == '__main__':
-    x = ParameterWatcher()
-    pkg1 = {'parameter_type': ParameterType.MODEL, 'operation_type': ParameterHandlerOperation.INSERT}
-    pkg2 = {'parameter_type': ParameterType.DATA, 'operation_type': ParameterHandlerOperation.DELETE}
-    pkg3 = {'parameter_type': ParameterType.TRANINING, 'operation_type': ParameterHandlerOperation.UPDATE}
-    pkg4 = {'parameter_type': ParameterType.MISCELLANEOUS, 'operation_type': ParameterHandlerOperation.SELECT}
-    pkgs = [pkg1, pkg2, pkg3, pkg4]
+    x = ParameterWatcher('test')
+    pkgs = [{'parameter_type': ParameterType.MODEL, 'operation_type': ParameterHandlerOperation.INSERT, 'insert_keys':['time'], 'time': [100, 200, 300]} for i in range(100)]
+    # pkg2 = {'parameter_type': ParameterType.DATA, 'operation_type': ParameterHandlerOperation.DELETE}
+    # pkg3 = {'parameter_type': ParameterType.TRANINING, 'operation_type': ParameterHandlerOperation.UPDATE}
+    # pkg4 = {'parameter_type': ParameterType.MISCELLANEOUS, 'operation_type': ParameterHandlerOperation.SELECT}
     for pkg in pkgs:
         x.main_parameter_handler(pkg)
+    time.sleep(5)
+    ParameterWatcher.terminate_save_proc()
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# PythonUtils
-
-## QUICK TEST
-```
-python -m rutils.Archive.Transaction
-```
+# nlutils
+This toolkit has the following features
+- Colorful logs based on logging and coloredlogs
+- Visual components, can easily draw different types of fugyres
+- ParameterWatcher, save all parameters to json files
+## Requirements
+- matplotlib
+- coloredlogs
+- torch
+- numpy
