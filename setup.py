5cf8b8d6b1b7c1b5a63810bd6271944d4e56d7af
Small-dragon <nikolaliu@icloud.com>
2022-09-10 15:15:05 -0400

    Update release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 from distutils.core import setup
 
 setup(name = "nlutils-mini",
-    version = "0.0.5",
+    version = "0.0.6",
     description = "Toolkit for neural learning training",
     author = "Nikola Liu",
     author_email = "nikolaliu@icloud.com",
